03fe38633e3c59a0bb3b33bec5f1b57e5facb7a6
kylecutchens <60196265+kylecutchens@users.noreply.github.com>
2021-10-20 17:24:45 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<title> IST 470 Campus Directions </title>
+	<meta charset='utf-8'>
+<header style = "text-align: center; background-color: #565657; font: large Arial">
+
+<h1> IST470 Campus Directions Project </h1>
+
+ </header>
+
+<?php
+	$con = new mysqli('localhost', 'root', 'PC5X6e4qejsK3s', 'ist470');
+	if ($con->connect_error)
+	{
+		die('could not connect to mySQL: ' . $con->connect_error);
+	}
+	else
+	//	echo "Successfully Connected to Database. ";
+
+
+	$centerData = $con->query("SELECT * FROM coordinates WHERE bcode = 'UC'");
+
+	if ($centerData)
+	{
+		$center = $centerData->fetch_assoc();
+	//	echo "Successfully Retreived Center Data. ";
+	}
+	else
+		die("SELECT CENTER FAILED");
+?>
+</head>
+
+<body style = 'font: Impact, sans-serif; background-color:#b3b3b3; text-align:center'>
+
+<div id = 'googleMap' style = "width:100%; height:700px; font: sans-serif "></div>
+
+<!--
+	<script>
+	function myMap()
+	{
+
+		var mapProp=
+		{
+
+			center:new google.maps.LatLng(<?php echo $center['lat'] . ", " . $center['Lon']?>),
+				zoom:6,
+
+		};
+
+		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+
+	-->
+	
+
+	<script>
+	function myMap()
+	{
+
+		var mapProp=
+		{
+
+			center:new google.maps.LatLng('32.8275', '-83.6494'),
+				zoom:16,
+
+		};
+
+		var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+
+	<?php
+
+	$markerData = $con->query("SELECT * FROM markers WHERE type = 'm'");
+
+	if($markerData)
+	{
+		while($row = $markerData->fetch_assoc())
+		{
+	
+			
+			echo "var m" . $row['ID'] . " =new google.maps.Marker({position:new google.maps.LatLng(".$row['Lat'] . ", " . $row['Lon'] . ")});\n";
+			echo "m" . $row['ID'] . ".setMap(map);\n";
+			echo "var i" . $row['ID'] . " = new google.maps.InfoWindow({content: '".htmlentities($row['Description']) . "'});\n";
+			echo "m" . $row['ID'] . ".addListener('click', function()  {i" . $row['ID'] . ".open(map, m" . $row['ID'] . ");});\n";
+
+	
+		//	echo "Successfully Retreived Map Marker Data";
+		}
+
+
+	}
+	
+	?>
+	
+	}
+
+	</script>
+	
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCA8OXkLqtVF7X51RpqSBL5MjTPZqTNdIo&callback=myMap"></script>
+<!--
+<div style = "border: 5px solid black; font: large Arial; background-color:#565657"> 
+<p> Click the link below to access the Marker Entry Form </p>
+<a href="data.php">Marker Data Entry Form</a> 
+</div>
+-->
+</body>
+
+
+
+</html>
+
+
